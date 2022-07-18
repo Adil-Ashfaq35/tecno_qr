0595398c4bf9b04c7e0a670f9258aba989ee43c8
farhanali-98 <fahranali2301@gmail.com>
2022-07-18 18:07:52 +0500

    initial comment

--- lib/views/generate_qr.dart
+++ lib/views/generate_qr.dart
@@ -6,7 +6,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:gallery_saver/gallery_saver.dart';
-import 'package:get/get.dart';
+
 
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -97,8 +97,8 @@ class CreateQrPage extends StatelessWidget {
               icon: CupertinoIcons.camera,
               optionText: 'Download',
               onTap: () {
-                
                 takeScreenShot();
+
               },
             ),
             OptionsWidget(
